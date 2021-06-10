8997961aad312a32f1a37437969c3cca58dd6be6
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-10 18:19:58 +1200

    Move get-connected to a component

---
+++ inc/components/home/get-connected.php
@@ -0,0 +1,16 @@
+    <div class="row justify-content-center">
+      <div class="get-connected col-11 col-md-10 col-xl-8 col-xxl-7 text-white p-4">
+        <div class="row">
+          <div class="col-md-7 mb-3 mb-md-0 d-flex">
+            <div class="get-connected-text">GET CONNECTED</div>
+          </div>
+          <div class="col-md-5">
+            <div class="play-now-btn-hover">
+              <a href="#" class="play-now-btn">
+                <div class="play-now-btn-text">PLAY NOW</div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
--- index.php
+++ index.php
@@ -8,22 +8,7 @@
     </video>
   </div>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="get-connected col-11 col-md-10 col-xl-8 col-xxl-7 text-white p-4">
-        <div class="row">
-          <div class="col-md-7 mb-3 mb-md-0 d-flex">
-            <div class="get-connected-text">GET CONNECTED</div>
-          </div>
-          <div class="col-md-5">
-            <div class="play-now-btn-hover">
-              <a href="#" class="play-now-btn">
-                <div class="play-now-btn-text">PLAY NOW</div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php include('inc/components/home/get-connected.php') ?>
   </div>
 </div>
 
@@ -169,22 +154,7 @@
         </div>
       </div>
     </div>
-    <div class="row justify-content-center">
-      <div class="get-connected col-11 col-md-10 col-xl-8 col-xxl-7 text-white p-4">
-        <div class="row">
-          <div class="col-md-7 mb-3 mb-md-0 d-flex">
-            <div class="get-connected-text">GET CONNECTED</div>
-          </div>
-          <div class="col-md-5">
-            <div class="play-now-btn-hover">
-              <a href="#" class="play-now-btn">
-                <div class="play-now-btn-text">PLAY NOW</div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php include('inc/components/home/get-connected.php') ?>
   </div>
 </div>
 
