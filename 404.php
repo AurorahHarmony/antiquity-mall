f90aa2b6bd49ffbb139bb9fcb65502ac7da00382
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-10 18:23:39 +1200

    Create 404 page and cache rules

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php require_once('inc/templates/header.php') ?>
+
+<div class="container py-5">
+  <div class="py-5">
+    <h1>Page Not Found 🐇</h1>
+    <p>That's no good... <a href="/">Go back home</a> where it's safe and sound!</p>
+  </div>
+</div>
+<?php require_once('inc/templates/footer.php') ?>
