e20c7a03071abea9c26a67eb8ae5f37dfa38cec2
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-12 21:29:56 +1200

    Add collation rules to columns to make some case sensitive

--- sql/users.sql
+++ sql/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
   id SERIAL,
-  username VARCHAR(20) NOT NULL UNIQUE,
-  email VARCHAR(320) NOT NULL UNIQUE,
+  username VARCHAR(20) NOT NULL UNIQUE COLLATE utf8mb4_bin,
+  email VARCHAR(320) NOT NULL UNIQUE COLLATE utf8mb4_general_ci,
   birthdate DATE NOT NULL,
-  password VARCHAR(60) NOT NULL
+  password VARCHAR(60) NOT NULL COLLATE utf8mb4_bin
 )
