a89704e14918699d56d9b86c17b87c129932ce5a
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-15 10:28:37 +1200

    Refactor and add update function

--- inc/services/PostService.php
+++ inc/services/PostService.php
@@ -12,37 +12,25 @@ class PostService
    */
   static function create(Form $form)
   {
-    $title = trim($form->get_value('title'));
-    $user_id = $form->get_value('user_id');
-    $content = $form->get_value('content');
-
-    if (empty($title)) {
-      $form->add_error('title', 'The post must have a title');
-    }
-
-    if (empty($content)) {
-      $form->add_error('content', 'You can not make an empty post!');
-    }
+    self::validate_post_fields($form);
 
     if ($form->has_errors()) {
       return false;
     }
 
-    //Sanitize the Title
-    $sanitized_title = htmlspecialchars($title);
+    $sanitized_input = self::sanitize_post_fields($form);
+    $san_title = $sanitized_input['title'];
+    $san_content = $sanitized_input['content'];
 
-    //Sanitize the Content
-    $allowedTags = '<p><strong><em><u><h1><h2><h3><h4><h5><h6><img>';
-    $allowedTags .= '<li><ol><ul><span><div><br><ins><del>';
-    $sanitized_content = strip_tags(stripslashes($content), $allowedTags);
+    $user_id = $form->get_value('user_id');
 
     //Create the Post
     try {
       $db = new Database;
       $db->insert('posts', [
-        'title' => $sanitized_title,
+        'title' => $san_title,
         'author_id' => $user_id,
-        'content' => $content
+        'content' => $san_content
       ]);
 
       return true;
@@ -52,10 +40,97 @@ class PostService
     }
   }
 
+  /**
+   * @param int $post_id The ID of the post that you want to update
+   * @param Form $form a Form object with a title and content key
+   * @return bool True if the post is sucessfully updated
+   */
+  public static function update($post_id, Form $form)
+  {
+    self::validate_post_fields($form);
+
+    if ($form->has_errors()) {
+      return false;
+    }
+
+    $sanitized_input = self::sanitize_post_fields($form);
+    $san_title = $sanitized_input['title'];
+    $san_content = $sanitized_input['content'];
+
+    //Update the post
+    try {
+      $db = new Database;
+      $db->update('posts', ['title' => $san_title, 'content' => $san_content], 'id = :post_id', ['post_id' => $post_id]);
+      return true;
+    } catch (\Throwable $th) {
+      $form->general_error = 'Internal Server Error - ' . $th;
+      return false;
+    }
+  }
+
+  /**
+   * @param Form $form The form object to be validated
+   */
+  private static function validate_post_fields(Form $form)
+  {
+    $title = trim($form->get_value('title'));
+    $content = $form->get_value('content');
+
+    if (empty($title)) {
+      $form->add_error('title', 'The post must have a title');
+    }
+
+    if (empty($content)) {
+      $form->add_error('content', 'You can not make an empty post!');
+    }
+  }
+
+  /**
+   * Sanitizes the title and content fields for a form
+   * @param Form $form The form object to sanitize
+   * @return array ['sanitized_title', 'sanitized_content']
+   */
+  private static function sanitize_post_fields(Form $form)
+  {
+    //Sanitize the Title
+    $title = trim($form->get_value('title'));
+    $sanitized_title = htmlspecialchars($title);
+
+    //Sanitize the Content
+    $content = $form->get_value('content');
+    $allowedTags = '<p><strong><em><u><h1><h2><h3><h4><h5><h6><img>';
+    $allowedTags .= '<li><ol><ul><span><div><br><ins><del>';
+    $sanitized_content = strip_tags(stripslashes($content), $allowedTags);
+
+    return [
+      'title' => $sanitized_title,
+      'content' =>  $sanitized_content
+    ];
+  }
+
+  /**
+   * Gets all posts
+   * @return array of All posts
+   */
   public static function get_all()
   {
     $db = new Database;
     $all_posts = $db->select('SELECT * FROM posts');
     return $all_posts;
+  }
+
+  /**
+   * @param int $post_id The ID of the post that you want to find
+   * @return array|bool False if no post was found
+   */
+  public static function get_one($post_id)
+  {
+    $db = new Database;
+    $found_post = $db->select('SELECT * FROM posts WHERE id = :id', ['id' => $post_id]);
+    if (count($found_post) > 0) {
+      return $found_post[0];
+    } else {
+      return false;
+    }
   }
 }
