13bd58d56037776114532304fc6932527312c308
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-15 11:08:34 +1200

    Add basic post loop to news page

--- news/index.php
+++ news/index.php
@@ -1,7 +1,10 @@
 <?php
 $title = 'News';
 
-require_once('../inc/templates/header.php')
+require_once('../inc/templates/header.php');
+
+require_once(__DIR__ . '/../inc/services/PostService.php');
+$all_posts = PostService::get_all();
 ?>
 
 <div class="container py-5">
@@ -9,14 +12,15 @@ require_once('../inc/templates/header.php')
     <h1>News</h1>
     <div class="row">
       <div class="col-md-8 col-lg-9">
+        <?php foreach ($all_posts as $post) : ?>
         <div class="article-preview">
-          <h2 class="mb-0">Article Name</h2>
-          <span class="badge gradient-pinkorange mb-1">1 Jan, 2020</span>
-          <p>Excerpt... Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic harum blanditiis culpa explicabo id
-            quam voluptate reprehenderit, pariatur fugit, sapiente error deleniti aliquam ducimus perferendis est,
-            facere modi tenetur! Officia. <a href="/news/article?id=1">[Read More]</a></p>
+          <h2 class="mb-0 text-capitalize"><?= $post['title'] ?></h2>
+          <span
+            class="badge gradient-pinkorange mb-1"><?= date_format(new DateTime($post['publish_date']), 'd M, Y') ?></span>
+          <p><?= $post['content'] ?><a href="/news/article?id=<?= $post['id'] ?>">[Read More]</a></p>
         </div>
         <hr>
+        <?php endforeach; ?>
 
         <nav aria-label="Page Navigation">
           <ul class="pagination">
