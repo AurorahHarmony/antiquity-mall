377efea2fc5386f88586809a42b3d7d76a96b84a
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-15 11:29:20 +1200

    Make single article page dynamic

--- news/article.php
+++ news/article.php
@@ -1,16 +1,26 @@
 <?php
-$title = 'Article';
 
-require_once('../inc/templates/header.php')
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id'])) {
+  require_once(__DIR__ . '/../inc/services/PostService.php');
+  $post_data = PostService::get_one(trim($_GET['id']));
+
+  if ($post_data == false) {
+    header('location: /404');
+    exit;
+  }
+} else {
+  header('location: /404');
+  exit;
+}
+
+$title = 'Article';
+require_once('../inc/templates/header.php');
 ?>
 
 <div class="container py-5">
   <div class="py-5">
-    <h1>Article</h1>
-    <p>Welcome to article <?php echo $_GET['id'] ?></p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, totam eaque. Tempora, aliquam eius. Rem quia,
-      eveniet commodi aspernatur tempore quaerat suscipit velit. Voluptas molestiae ipsa cumque odio soluta praesentium?
-    </p>
+    <h1 class="text-capitalize"><?= $post_data['title'] ?></h1>
+    <?= $post_data['content'] ?>
   </div>
 </div>
 <?php require_once('../inc/templates/footer.php') ?>
