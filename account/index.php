1a1edddc4c2075fe03c2a3d1b96ce0832dfc3810
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-15 15:43:31 +1200

    Temporarily print out role and perms

--- account/index.php
+++ account/index.php
@@ -11,6 +11,16 @@ include(__DIR__ . '/../inc/templates/account/start.php');
 
 <p>Welcome, <b><?php echo $_SESSION['username'] ?? 'username' ?></b>.</p>
 
+
+<?php
+require_once(__DIR__ . '/../inc/services/PermissionService.php');
+
+$user_role = PermissionService::get_role($_SESSION['id']);
+$user_perms = implode(", ", PermissionService::get_perms($_SESSION['id']));
+?>
+<p>Your Role is <?= $user_role ?></p>
+<p>Your Permissions are <?= $user_perms ?></p>
+
 <?php
 include(__DIR__ . '/../inc/templates/account/end.php');
 require_once(__DIR__ . '/../inc/templates/footer.php')
