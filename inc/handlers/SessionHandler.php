ba167fb272588cb88823b3240d43210cbce2ece9
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-16 21:50:24 +1200

    Adjust session handler to be able to check perms

--- inc/handlers/SessionHandler.php
+++ inc/handlers/SessionHandler.php
@@ -8,12 +8,34 @@ class Session
     }
   }
 
-  public function protected_route()
+  /**
+   * @param string $required_perm A permission that is required to view this route
+   * @param bool $get_fresh Should we force the system to get fresh user perms from the database?
+   */
+
+  public function protected_route($required_perm = null, $get_fresh = false)
   {
-    if ($_SESSION['logged_in'] != true || empty($_SESSION['id'])) {
-      http_response_code(403);
-      header('location: /login');
-      exit;
+
+    if ($required_perm == null) {
+      if ($_SESSION['logged_in'] != true || empty($_SESSION['id'])) {
+        http_response_code(403);
+        header('location: /login');
+        exit;
+      }
+    } else {
+      if ($_SESSION['logged_in'] != true || empty($_SESSION['id'])) {
+        http_response_code(404);
+        header('location: /404');
+        exit;
+      } else {
+        require_once(__DIR__ . '/../services/PermissionService.php');
+        $user_perms = PermissionService::has_perm($required_perm, $_SESSION['id'], $get_fresh);
+        if ($user_perms == false) {
+          http_response_code(404);
+          header('location: /404');
+          exit;
+        }
+      }
     }
   }
 
@@ -35,7 +57,7 @@ class Session
     //Check if the User is allowed to login
     require_once(__DIR__ . '/../services/PermissionService.php');
 
-    $user_perms = PermissionService::has_perm($user_id, 'LOGIN', true);
+    $user_perms = PermissionService::has_perm('LOGIN', $user_id, true);
     if (!$user_perms) {
       throw new Error("You do not have permission to login.");
     }
@@ -43,6 +65,6 @@ class Session
     $_SESSION['id'] = $user_id;
     $_SESSION['logged_in'] = true;
     $_SESSION['username'] = $username;
-    $_SESSION['perms'] = $perms;
+    $_SESSION['perms'] = $user_perms;
   }
 }
