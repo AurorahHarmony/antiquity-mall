f9761d1c5f9e85e3a59a206633ddaa716e74f62d
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-17 13:08:41 +1200

    fix page titles

--- manage/users/edit-roles.php
+++ manage/users/edit-roles.php
@@ -3,7 +3,7 @@ require_once(__DIR__ . '/../../inc/handlers/SessionHandler.php');
 $session = new Session;
 $session->protected_route('ACCESS_MANAGER', true);
 
-$title = 'Site Dashboard';
+$title = 'Edit Roles';
 require_once(__DIR__ . '/../../inc/templates/manage/start.php');
 ?>
 
