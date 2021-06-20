ac6038fce3e3e10a08e3646a972a161f0c8e51c3
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-21 11:19:51 +1200

    Add privacy policy

--- privacy-policy.php
+++ privacy-policy.php
@@ -7,8 +7,29 @@ require_once('inc/templates/header.php')
 <div class="container py-5">
   <div class="py-5">
     <h1>Privacy Policy</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore maiores iusto nihil doloremque autem quia dicta
-      velit eius, asperiores facere iste minus cumque tenetur quo suscipit ex, eos similique modi.</p>
+    <p>We collect personal information from you, including information about your:
+    <ul>
+      <li>Contact information</li>
+      <li>Billing or purchase information</li>
+      <li>IP Address</li>
+      <li>Date of Birth</li>
+    </ul>
+    </p>
+
+    <p>We collect your personal information in order to:
+    <ul>
+      <li>Manage Accounts and the Login System</li>
+      <li>Moderate user generated content</li>
+      <li>Age restrict access to our services</li>
+    </ul>
+    </p>
+
+    <p>Providing some information is optional. If you choose not to enter your email and date of birth, we'll be unable
+      to provide access to Dreamscape and our services.</p>
+
+    <p>You have the right to ask for a copy of any personal information we hold about you, and to ask for it to be
+      corrected if you think it is wrong. If you’d like to ask for a copy of your information, or to have it corrected,
+      please contact us at scriptronixstudios@gmail.com.</p>
   </div>
 </div>
 <?php require_once('inc/templates/footer.php') ?>
