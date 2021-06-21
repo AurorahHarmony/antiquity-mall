f3168c8ba2b8f02c1c292c6fe50a24e8b6d28924
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-22 09:39:14 +1200

    Add basic about page content

--- about.php
+++ about.php
@@ -7,8 +7,8 @@ require_once('inc/templates/header.php')
 <div class="container py-5">
   <div class="py-5">
     <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore maiores iusto nihil doloremque autem quia dicta
-      velit eius, asperiores facere iste minus cumque tenetur quo suscipit ex, eos similique modi.</p>
+    <p>Dreamscape is a virtual social space with access to many interactive activities. Within the world of Dreamscape,
+      you can explore arcades, shopping districts, minigames, collectables, customizable living spaces and more.</p>
   </div>
 </div>
 <?php require_once('inc/templates/footer.php') ?>
