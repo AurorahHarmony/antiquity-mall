b37650680db191580f9f04d39fc349a76991c9aa
Aurorah Harmony <aurorahHarmony@gmail.com>
2021-06-23 23:53:29 +1200

    Add basic Q&A

--- help.php
+++ help.php
@@ -7,8 +7,39 @@ require_once('inc/templates/header.php')
 <div class="container py-5">
   <div class="py-5">
     <h1>Help</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore maiores iusto nihil doloremque autem quia dicta
-      velit eius, asperiores facere iste minus cumque tenetur quo suscipit ex, eos similique modi.</p>
+    <p>
+      <b>Q. How do I download the game?</b>
+      <br>
+      A. Login and Head to the <a href="/account/downloads">Downloads</a> page. Then select the correct version for your
+      operating system.
+    </p>
+
+    <p>
+      <b>Q. I can't login?</b>
+      <br>
+      A. If you're unable to login, double check that you've got your credentials correct. If not, you can contact our
+      support team to initiate a password reset.
+    </p>
+
+    <p>
+      <b>Q. I want to support development. How can I help with that?</b>
+      <br>
+      A. We'd love to hear your suggestions. You can send them to us on <a href="https://twitter.com/theDreamscape"
+        target="__blank" rel="noopener">Twitter</a>. You
+      can also donate on <a href="/external">Patreon</a>.
+    </p>
+
+    <p>
+      <b>Q. Are you hiring?</b>
+      <br>
+      A. Not at the moment, but watch this space!
+    </p>
+
+    <p>
+      <b>Q. I've been banned, what can I do?</b>
+      <br>
+      A. Unfortunately, we are unable to offer support for unbanning users at this time.
+    </p>
   </div>
 </div>
 <?php require_once('inc/templates/footer.php') ?>
